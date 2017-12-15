22e0765f7f225e974667bcb5ad6bc2cab15c2f6a
chencm <chaoming007@163.com>
2017-12-15 19:47:40 +0800

    first commit

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-class App extends React.Component{
-    render(){
-        return(
-            <div className="box">欢迎来到react的世界！</div>
-        )
-    }
-}
-export default App
