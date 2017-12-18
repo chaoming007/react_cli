85e253b4fa88d3a5c472fae8eeee68df0fbc5d4f
chencm <chaoming007@163.com>
2017-12-18 13:20:23 +0800

    增加版本检查

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const download = require('download-git-repo')
 const ora = require('ora')
 const fs = require('fs-extra')
 
+let checkVersion=require("./lib/check-version");
+let packAge = require(__dirname + '/package.json')
 let questions=require("./lib/questions")
 let tempJson=path.join(__dirname,"/template/package.json")
 let temp=path.join(__dirname,"/template")
@@ -16,58 +18,71 @@ let filename="react-app"
 let spinner,objName;
 
 
+program.on('--help', () => {
+  console.log('  Examples:')
+  console.log()
+  console.log(chalk.gray('    # 创建一个新的react项目模板！（如果没有输入项目名称，默认名称为react-app）'))
+  console.log('    $ react init my-project')
+  console.log()
+})
 
-program.version('0.0.1');                                   
+
+program.version(packAge.version);                                   
 program
 .usage('<command> [options]')
 .command('init')                                        
-.description('create react working directory')          
+.description('创建一个新的react项目模板！')          
 .option('name', '项目的文件夹名字，如果没有默认为react-app')   
-.action(function (options) {                    
-       if(typeof options=="string"){
-         filename=options;
-         objName=path.join(process.cwd(),"/"+options)
-       }else{
-         objName=nowTemp;
-       }
-
-      inquirer.prompt(questions).then(answers => {
-        spinner= ora('downloading template');
-        spinner.start();
-        setFileFun(answers)
-      })
-      function setFileFun(obj){   
-          let datObj="";
-          fs.readJson(tempJson)
-          .then((packageObj) => {    
-            datObj=setPageJsonFun(packageObj,obj);
-            return fs.copy(temp,objName)
-          })
-          .then(()=>{
-            return fs.writeJson(path.join(objName,"package.json"),datObj);  
-          }).then(()=>{
-             spinner.stop()
-             f()
-          })
-      }
-      function setPageJsonFun(packageObj,obj){         
-           for(let key in packageObj){
-               for(let key_1 in obj){
-                  if(key===key_1){
-                     packageObj[key]=obj[key_1];
-                  }
-               }
-           }
-           return packageObj;
-      }
+.action(function (options) {      
+  checkVersion(setTempFun,options);              
       
-
 });
-program.parse(process.argv);                              
+program.parse(process.argv);     
+
+function setTempFun(options){
+      if(typeof options=="string"){
+        filename=options;
+        objName=path.join(process.cwd(),"/"+options)
+      }else{
+        objName=nowTemp;
+      }
+
+    inquirer.prompt(questions).then(answers => {
+      spinner= ora('downloading template');
+      spinner.start();
+      setFileFun(answers)
+    })
+    function setFileFun(obj){   
+        let datObj="";
+        fs.readJson(tempJson)
+        .then((packageObj) => {    
+          datObj=setPageJsonFun(packageObj,obj);
+          return fs.copy(temp,objName)
+        })
+        .then(()=>{
+          return fs.writeJson(path.join(objName,"package.json"),datObj);  
+        }).then(()=>{
+            spinner.stop()
+            f()
+        })
+    }
+    function setPageJsonFun(packageObj,obj){         
+          for(let key in packageObj){
+              for(let key_1 in obj){
+                if(key===key_1){
+                    packageObj[key]=obj[key_1];
+                }
+              }
+          }
+          return packageObj;
+    }
+
+}
+
 function f(){
     console.log(" ");
-    console.log("项目创建完成...");
+    console.log("    项目创建完成...");
     console.log(" ");
-    console.log("请执行",chalk.red("cd "+filename+" && npm install"),"进行开发吧...");
+    console.log("    请执行",chalk.red("cd "+filename+" && npm install"),"进行开发吧...");
 }
 
